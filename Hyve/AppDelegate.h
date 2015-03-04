d3919b7fd897c743414e27feb1911322863566ed
VLT Labs <vltlabs@VLTs-MacBook-Pro.local>
2015-03-04 18:24:34 +0800

    Initial Commit

---
+++ Hyve/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hyve
+//
+//  Created by VLT Labs on 3/4/15.
+//  Copyright (c) 2015 Jay Ang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

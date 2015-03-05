7fa449513821f0ab32a6be7a641258673afc1bc4
VLT Labs <vltlabs@VLTs-MacBook-Pro.local>
2015-03-05 09:23:23 +0800

    Integrate pop into project

---
+++ Hyve/DashboardViewController.h
@@ -0,0 +1,13 @@
+//
+//  DashboardViewController.h
+//  Hyve
+//
+//  Created by VLT Labs on 3/5/15.
+//  Copyright (c) 2015 Jay Ang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DashboardViewController : UIViewController
+
+@end
---
+++ Hyve/DashboardViewController.m
@@ -0,0 +1,31 @@
+//
+//  DashboardViewController.m
+//  Hyve
+//
+//  Created by VLT Labs on 3/5/15.
+//  Copyright (c) 2015 Jay Ang. All rights reserved.
+//
+
+#import "DashboardViewController.h"
+
+@interface DashboardViewController ()
+
+@property (weak, nonatomic) IBOutlet UIButton *hyveButton;
+@end
+
+
+@implementation DashboardViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+
+- (IBAction)onHyveButtonPressed:(id)sender
+{
+    
+}
+
+
+@end

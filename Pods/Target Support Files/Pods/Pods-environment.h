58102f4a0cb8d2b55515eaa28d3f8d3921ee615c
VLT Labs <vltlabs@VLTs-MacBook-Pro.local>
2015-03-18 16:10:04 +0800

    reinstall cocoapods, added DKCircleButton

---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,26 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// CNPGridMenu
+#define COCOAPODS_POD_AVAILABLE_CNPGridMenu
+#define COCOAPODS_VERSION_MAJOR_CNPGridMenu 0
+#define COCOAPODS_VERSION_MINOR_CNPGridMenu 2
+#define COCOAPODS_VERSION_PATCH_CNPGridMenu 1
+
+// DKCircleButton
+#define COCOAPODS_POD_AVAILABLE_DKCircleButton
+#define COCOAPODS_VERSION_MAJOR_DKCircleButton 1
+#define COCOAPODS_VERSION_MINOR_DKCircleButton 1
+#define COCOAPODS_VERSION_PATCH_DKCircleButton 0
+
+// pop
+#define COCOAPODS_POD_AVAILABLE_pop
+#define COCOAPODS_VERSION_MAJOR_pop 1
+#define COCOAPODS_VERSION_MINOR_pop 0
+#define COCOAPODS_VERSION_PATCH_pop 7
+

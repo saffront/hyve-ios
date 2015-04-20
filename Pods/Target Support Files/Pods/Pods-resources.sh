3f76e2155794d3baf0f4a1dd1b8c707590518653
VLT Labs <vltlabs@VLTs-MacBook-Pro.local>
2015-04-20 14:34:11 +0800

    Integrate kvnprogress framework into app

--- Pods/Target Support Files/Pods/Pods-resources.sh
+++ Pods/Target Support Files/Pods/Pods-resources.sh
@@ -61,6 +61,7 @@ install_resource()
                     install_resource "AWSSNS/AWSSNS/Resources/sns-2010-03-31.json"
                     install_resource "AWSSQS/AWSSQS/Resources/sqs-2012-11-05.json"
                     install_resource "AWSSimpleDB/AWSSimpleDB/Resources/sdb-2009-04-15.json"
+                    install_resource "KVNProgress/KVNProgress/Resources/KVNProgressView.xib"
                     install_resource "${BUILT_PRODUCTS_DIR}/MBSimpleLoadingIndicator.bundle"
           
 rsync -avr --copy-links --no-relative --exclude '*/.svn/*' --files-from="$RESOURCES_TO_COPY" / "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
